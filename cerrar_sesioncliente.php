b59acbd1d1012cc1d60192fdd3ea4ddef7c2b2fa
guzman.valle.adrian.16 <guzman.valle.adrian.16@gmail.com>
2023-11-14 08:28:28 -0500

    Modificaciones

--- cerrar_sesioncliente.php
+++ cerrar_sesioncliente.php
@@ -9,16 +9,55 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmar Cierre de Sesión</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            margin-top: 100px;
+        }
+
+        .confirm-box {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .btn-aceptar,
+        .btn-cancelar {
+            padding: 10px 20px;
+            margin: 10px;
+            cursor: pointer;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+        }
+
+        .btn-aceptar {
+            background-color: blue;
+            color: white;
+        }
+
+        .btn-cancelar {
+            background-color: #f44336;
+            color: #fff;
+        }
+    </style>
 </head>
 
 <body>
+    <div class="confirm-box">
+        <h2>¿Estás seguro de que deseas cerrar la sesión?</h2>
+        <button class="btn-aceptar" onclick="confirmarCierreSesion(true)">Aceptar</button>
+        <button class="btn-cancelar" onclick="confirmarCierreSesion(false)">Cancelar</button>
+    </div>
+
     <script>
-        // Función para mostrar la ventana de confirmación
-        function confirmarCierreSesion() {
-            // Ventana emergente de confirmación
-            var confirmacion = confirm("¿Estás seguro de que deseas cerrar la sesión?");
-            if (confirmacion) {
-                // Si el usuario hace clic en "Aceptar", destruir la sesión y redirigir
+        function confirmarCierreSesion(aceptar) {
+            if (aceptar) {
                 <?php
                 // Destruir todas las variables de sesión
                 session_unset();
@@ -34,9 +73,6 @@ session_start();
                 window.location.href = "menucliente.php";
             }
         }
-
-        // Llamar a la función cuando la página se carga
-        window.onload = confirmarCierreSesion;
     </script>
 </body>
 
